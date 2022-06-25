bf788a54d956e9eebb4823df1a8ea928e418eb30
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-06-25 15:03:07 +0900

    JSONとして取り出し

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+"""
+郵便番号7桁(0287111)を入力したら
+岩手県八幡平市大更が出力される
+
+python app.py
+郵便番号<ハイフン無し>は？ 0287111
+岩手県八幡平市大更
+"""
+
 import requests
 
-response = requests.get("https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060")
+pos_code = input("郵便番号<ハイフン無し>は？")
+pos_code = "0287111"
 
-print(response)
-print(response.text)
+response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={pos_code}")
+
+dic = response.json()
+print(dic)
+
