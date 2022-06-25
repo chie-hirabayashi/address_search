e5841c730e6f75b8dccb4cd967f10ce8f3054441
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-06-25 15:55:36 +0900

    住所を表示できるようにした

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ python app.py
 
 import requests
 
-pos_code = input("郵便番号<ハイフン無し>は？")
+# pos_code = input("郵便番号<ハイフン無し>は？")
 pos_code = "0287111"
 
 response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={pos_code}")
@@ -17,3 +17,9 @@ response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={pos_
 dic = response.json()
 print(dic)
 
+# print(f"{dic['results'][0]['address1']}{dic['results'][0]['address2']}{dic['results'][0]['address3']}")
+pref_name = dic['results'][0]['address1']
+city_name = dic['results'][0]['address2']
+town_name = dic['results'][0]['address3']
+
+print(f"{pref_name}{city_name}{town_name}")
